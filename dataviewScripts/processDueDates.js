d798a2988d8175daf48bce506edaa9145bece86b
Tucker Bradford <tucker@tuckerbradford.com>
2025-03-10 09:05:55 -0400

    updated processDueDates

--- dataviewScripts/processDueDates.js
+++ dataviewScripts/processDueDates.js
@@ -11,7 +11,7 @@ const processDueDates = async (dv, courseId) => {
     try {
       const file = await dv.app.vault.getFileByPath(path)
       const content = await dv.app.vault.read(file);
-      const regex = /## Due Dates([\s\S]*?)(?=\n#|$)/; 
+      const regex = /# Due Dates([\s\S]*?)(?=\n#|$)/; 
       const matches = content?.match(regex);
       if (matches) {
         const tableData = matches[1].trim(); 
@@ -22,21 +22,32 @@ const processDueDates = async (dv, courseId) => {
           .filter(c => c)
           .filter(Boolean)
 
-          let dueDate = columns[0]; 
-          let assignment = columns[1]; 
-          if (dueDate && assignments) { 
-            assignment = new Date(dueDate) < new Date() ? `<span stlye="color: #e3e3e0;">~~${assignment}~~</span>` : assignment
-            if (dueDate && assignment) {
-              allEntries.push([dueDate,assignment ]); 
+          let dueDate = columns[0] 
+          let assignment = columns[1] 
+          if (!Date.parse(dueDate)) {continue}
+          const uniqueRow = !allEntries.some(e => (e[0].match(moment(dueDate)?.format("YYYY-MM-DD")) && e[1] == assignment))
+          if (assignment && uniqueRow) { 
+            if (moment(dueDate) < moment()) {
+             continue 
             }
+            else if (moment(dueDate) < moment()?.add(1, "w")) {
+              dueDate = `<span style="background-color: #FF808D;">${moment(dueDate)?.format("YYYY-MM-DD")}</span>`
+            } else if (moment(dueDate) < moment().add(2, "w")) {
+              dueDate = `<span style="background-color: #FCFFA5; color: black;">${moment(dueDate)?.format("YYYY-MM-DD")}</span>`
+            }else {
+              dueDate = moment(dueDate)?.format("YYYY-MM-DD")
+            } 
+            allEntries.push([dueDate,assignment,`[[${path}]]` ]); 
           }
         }
       } 
     }catch(e) {console.log(e, page)}
   }
+  console.log(allEntries)
+  allEntries =  allEntries.sort((a,b) => moment(b[0]).unix() - moment(a[0]).unix() )
 
-
-  dv.table(["Due Date", "Task Description"], allEntries);
+  dv.table(["Due Date", "Task Description", "File"], 
+    allEntries.sort((a,b) => moment(b[0]).unix() - moment(a[0]).unix() ))
 }
 
 module.exports = {processDueDates}
